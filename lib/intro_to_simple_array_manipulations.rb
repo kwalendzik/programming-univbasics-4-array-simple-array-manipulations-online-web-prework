3e456f01fc85b88854e92593394a63afdebd404e
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-08 01:40:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,12 +15,12 @@ end
 
 def pop_with_args(array)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-    small_dogs = dog_breeds.pop(2)
+    small_dogs = array.pop(2)
 end 
 
 def using_shift(array)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  im_so_over_this_city = my_favorite_cities.shift
+  im_so_over_this_city = array.shift
   end
   
 def shift_with_args(array)
