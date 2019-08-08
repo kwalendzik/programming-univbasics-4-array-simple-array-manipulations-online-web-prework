a1940c71fc38e740b4576a6f08f07bc4bbe9b93f
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-08 00:36:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -54,3 +54,9 @@ def using_delete(array,string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   no_offense_steven = instructors.delete("Steven")
 end
+
+def using_delete_at(array,integer)
+  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+  deleted_robot = using_delete_at(famous_robots, 2)
+    expect(deleted_robot).to eq("Robocop")
+  end
