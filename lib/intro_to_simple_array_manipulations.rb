a451d672e908cfa3bc72505b4f98695bd3521ca4
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-08 01:29:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -51,7 +51,7 @@ def using_flatten(array)
 end
 
 def using_delete(array,string)
-  instructors.delete("Steven")
+  no_offense_steven = instructors.delete("Steven")
 end
 
 def using_delete_at(array,integer)
