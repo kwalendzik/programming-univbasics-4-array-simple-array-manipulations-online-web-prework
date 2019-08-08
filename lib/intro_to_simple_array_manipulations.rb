d7e64345cf1de4ee270df89df6823a13f0e89702
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-08 01:28:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -50,9 +50,9 @@ def using_flatten(array)
   flat_array = instruments.flatten
 end
 
-def using_delete(string)
+def using_delete(array,string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete("Steven")
+  instructors.delete("Steven")
 end
 
 def using_delete_at(array,integer)
