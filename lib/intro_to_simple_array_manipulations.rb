04d5ee70a207bf7d1ff833730978e519f70c8b82
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-08 00:37:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -57,6 +57,5 @@ end
 
 def using_delete_at(array,integer)
   famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-  deleted_robot = using_delete_at(famous_robots, 2)
-    expect(deleted_robot).to eq("Robocop")
-  end
+  deleted_robot = famous_robots.delete_at(2)
+end
