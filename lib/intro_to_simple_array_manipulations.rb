c487d777ea410e7238c1391dfaa9f2767fc5c1ea
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-08 00:19:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,5 +52,5 @@ end
 
 def using_delete(instructors)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = [instructors.delete("Steven")]
+  instructors.delete("Steven")
 end
